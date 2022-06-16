bd1c7665a153f1c435cdfed8b4f6ded9e8498cb9
Caleb Elliott <caleb123elliott@gmail.com>
2022-06-16 15:03:41 -0600

    changing more

--- src/components/Blog.vue
+++ src/components/Blog.vue
@@ -1,8 +1,13 @@
   <template>
   <div class="row d-flex justify-content-center">
-    <div class="col-6 d-flex">
-      <img class="img-fluid blog-img" src="" alt="">
-      <p>this is some sample text</p>
+    <div class="col-6 d-flex elevation-2 bg-light">
+      <img class="img-fluid blog-img"
+        src="https://www.rd.com/wp-content/uploads/2017/09/01-shutterstock_476340928-Irina-Bg.jpg" alt="">
+      <div class="mx-2">
+        <h4>Molly Elliott</h4>
+        <h5><u>Why I am a good triathlete</u></h5>
+        <p>This is some sample text from the body of the blog post</p>
+      </div>
     </div>
     <div class="col-2">
     </div>
